54a91a72202857bd78bfa5751d6a2d3c639bbc22
sc0000 <sebastiancyliax@gmx.net>
2024-11-22 12:01:43 +0100

    style pass

--- allocators/allocators.c
+++ allocators/allocators.c
@@ -14,12 +14,14 @@
 // if (!hdr)
 //   flog(LOG_WARNING, msg);
 
-bool is_pow2(uintptr_t p) {
+bool is_pow2(uintptr_t p)
+{
   uintptr_t mod = p & (p - 1);
   return mod == 0; 
 }
 
-bool within_bounds(void* ptr, unsigned char* buf, size_t buf_size) {
+bool within_bounds(void* ptr, unsigned char* buf, size_t buf_size)
+{
   uintptr_t uptr = (uintptr_t)ptr;
   uintptr_t ubuf = (uintptr_t)buf;
   uintptr_t uend = (ubuf + (uintptr_t)buf_size);
@@ -27,7 +29,8 @@ bool within_bounds(void* ptr, unsigned char* buf, size_t buf_size) {
   return (uptr > ubuf && uptr < uend);
 }
 
-uintptr_t align_ptr(uintptr_t ptr, uintptr_t align) {
+uintptr_t align_ptr(uintptr_t ptr, uintptr_t align)
+{
   static int call_count = 0;
 
   char* msg = NULL;
@@ -46,7 +49,8 @@ uintptr_t align_ptr(uintptr_t ptr, uintptr_t align) {
   return ptr;
 }
 
-uintptr_t align_size(size_t size, uintptr_t align) {
+uintptr_t align_size(size_t size, uintptr_t align)
+{
   size_t align_s = (size_t)align;
 
   static int call_count = 0;
@@ -67,7 +71,8 @@ uintptr_t align_size(size_t size, uintptr_t align) {
   return size;
 }
 
-void arena_init(arena_t* a, void* buf, size_t size) {
+void arena_init(arena_t* a, void* buf, size_t size)
+{
   a->size = size;
   a->curr_offset = 0;
   a->prev_offset = 0;
@@ -78,7 +83,8 @@ void* arena_alloc(arena_t* a, size_t size) {
   return arena_alloc_align(a, size, DEFAULT_ALIGN);
 }
 
-void* arena_alloc_align(arena_t* a, size_t size, uintptr_t align) {
+void* arena_alloc_align(arena_t* a, size_t size, uintptr_t align)
+{
   uintptr_t curr_ptr = (uintptr_t)(a->buf + a->curr_offset);
   uintptr_t offset_ptr = align_ptr(curr_ptr, align);
   offset_ptr -= (uintptr_t)a->buf;
@@ -96,11 +102,13 @@ void* arena_alloc_align(arena_t* a, size_t size, uintptr_t align) {
   return memset(ptr, 0, size);
 }
 
-void arena_resize_element(arena_t* a, void* element, size_t old_size, size_t new_size) {
+void arena_resize_element(arena_t* a, void* element, size_t old_size, size_t new_size)
+{
   arena_resize_element_align(a, element, old_size, new_size, DEFAULT_ALIGN);  
 }
 
-void arena_resize_element_align(arena_t* a, void* element, size_t old_size, size_t new_size, uintptr_t align) {
+void arena_resize_element_align(arena_t* a, void* element, size_t old_size, size_t new_size, uintptr_t align)
+{
   if (!element) {
     element = arena_alloc_align(a, new_size, align);
     return;
@@ -128,20 +136,24 @@ void arena_resize_element_align(arena_t* a, void* element, size_t old_size, size
   }
 }
 
-void arena_zero(arena_t* a) {
+void arena_zero(arena_t* a)
+{
   memset(a->buf, 0, a->size);
 }
 
-void  arena_pop(arena_t* a) {
+void  arena_pop(arena_t* a)
+{
   // TODO: Implement!
 }
 
-void arena_free(arena_t* a) {
+void arena_free(arena_t* a)
+{
   a->curr_offset = 0;
   a->prev_offset = 0;
 }
 
-uintptr_t align_ptr_hdr(uintptr_t ptr, uintptr_t align, size_t hdr_size) {
+uintptr_t align_ptr_hdr(uintptr_t ptr, uintptr_t align, size_t hdr_size)
+{
   if (!is_pow2(align)) {
     flog(LOG_ERROR, "Given alignment no power of 2");
     exit(EXIT_FAILURE);
@@ -169,18 +181,21 @@ uintptr_t align_ptr_hdr(uintptr_t ptr, uintptr_t align, size_t hdr_size) {
   return ptr + padding;
 }
 
-void stack_init(stack_t* s, void* buf, size_t size) {
+void stack_init(stack_t* s, void* buf, size_t size)
+{
   s->size = size;
   s->curr_offset = 0;
   s->curr_hdr = NULL;
   s->buf = (unsigned char*)buf;
 }
 
-void* stack_alloc(stack_t* s, size_t size) {
+void* stack_alloc(stack_t* s, size_t size)
+{
   return stack_alloc_align(s, size, DEFAULT_ALIGN);
 }
 
-void* stack_alloc_align(stack_t* s, size_t size, uintptr_t align) {
+void* stack_alloc_align(stack_t* s, size_t size, uintptr_t align)
+{
   uintptr_t curr_ptr = (uintptr_t)(s->buf + s->curr_offset);
   uintptr_t offset_ptr = align_ptr_hdr(curr_ptr, align, sizeof(hdr_t));
   offset_ptr -= (uintptr_t)s->buf;
@@ -203,11 +218,13 @@ void* stack_alloc_align(stack_t* s, size_t size, uintptr_t align) {
   return memset(ptr, 0, size);
 }
 
-void stack_resize_element(stack_t* s, void* element, size_t old_size, size_t new_size) {
+void stack_resize_element(stack_t* s, void* element, size_t old_size, size_t new_size)
+{
   stack_resize_element_align(s, element, old_size, new_size, DEFAULT_ALIGN);
 }
 
-void stack_resize_element_align(stack_t* s, void* element, size_t old_size, size_t new_size, uintptr_t align) {
+void stack_resize_element_align(stack_t* s, void* element, size_t old_size, size_t new_size, uintptr_t align)
+{
   if (!element) {
     stack_alloc_align(s, new_size, align);
     return;
@@ -237,22 +254,26 @@ void stack_resize_element_align(stack_t* s, void* element, size_t old_size, size
   }
 }
 
-void stack_pop(stack_t* s) {
+void stack_pop(stack_t* s)
+{
   if (!s->curr_hdr) return;
   s->curr_hdr = s->curr_hdr->linked_hdr;
   s->curr_offset = (uintptr_t)s->curr_hdr + sizeof(hdr_t); 
 }
 
-void stack_free_all(stack_t* s) {
+void stack_free_all(stack_t* s)
+{
   s->curr_hdr = NULL;
   s->curr_offset = 0;
 }
 
-void pool_init(pool_t* p, void* buf, size_t size, size_t slot_size) {
+void pool_init(pool_t* p, void* buf, size_t size, size_t slot_size)
+{
   pool_init_align(p, buf, size, slot_size, DEFAULT_ALIGN);
 }
 
-void pool_init_align(pool_t* p, void* buf, size_t size, size_t slot_size, uintptr_t align) {
+void pool_init_align(pool_t* p, void* buf, size_t size, size_t slot_size, uintptr_t align)
+{
   uintptr_t buf_zero = (uintptr_t)buf;
   uintptr_t buf_zero_aligned = align_ptr(buf_zero, align);
   size -= buf_zero_aligned - buf_zero;
@@ -266,7 +287,8 @@ void pool_init_align(pool_t* p, void* buf, size_t size, size_t slot_size, uintpt
   pool_free_all(p);
 }
 
-void* pool_alloc(pool_t* p) {
+void* pool_alloc(pool_t* p)
+{
   hdr_t* hdr = p->curr_hdr;
 
   if (!hdr) {
@@ -278,7 +300,8 @@ void* pool_alloc(pool_t* p) {
   return memset(hdr, 0, p->slot_size);
 }
 
-void pool_free(pool_t* p, void* slot) {
+void pool_free(pool_t* p, void* slot)
+{
   uintptr_t buf_int = (uintptr_t)p->buf;
   uintptr_t end_int = (uintptr_t)(p->buf + p->size);
   uintptr_t seg_int = (uintptr_t)slot;
@@ -293,7 +316,8 @@ void pool_free(pool_t* p, void* slot) {
   p->curr_hdr = hdr;
 }
 
-void pool_free_all(pool_t* p) {
+void pool_free_all(pool_t* p)
+{
   size_t slot_count = p->size / p->slot_size;
 
   for (size_t i = 0; i < slot_count; ++i) {
@@ -303,11 +327,13 @@ void pool_free_all(pool_t* p) {
   }
 }
 
-void free_list_init(free_list_t* fl, void* buf, size_t size) {
+void free_list_init(free_list_t* fl, void* buf, size_t size)
+{
   free_list_init_align(fl, buf, size, DEFAULT_ALIGN);
 }
 
-void free_list_init_align(free_list_t* fl, void* buf, size_t size, uintptr_t align) {
+void free_list_init_align(free_list_t* fl, void* buf, size_t size, uintptr_t align)
+{
   uintptr_t buf_zero = (uintptr_t)buf;
   uintptr_t buf_zero_aligned = align_ptr(buf_zero, align);
   uintptr_t diff = buf_zero_aligned - buf_zero; 
@@ -316,19 +342,22 @@ void free_list_init_align(free_list_t* fl, void* buf, size_t size, uintptr_t ali
   fl->size = size - diff;
 
   fl_hdr_t* first_hdr = fl_first_hdr(fl);
+  size_t hdr_size = align_size(sizeof(fl_hdr_t), align);
   first_hdr->linked_hdr = NULL;
-  first_hdr->block_size = size - sizeof(fl_hdr_t);
+  first_hdr->block_size = size - hdr_size;
 
   char msg[256];
   sprintf(msg, "Free list init. Size aligned to %llu (diff %llu)", size, diff);
   flog(LOG_INFO, msg);
 }
 
-void* free_list_alloc(free_list_t* fl, size_t size, fl_policy policy) {
+void* free_list_alloc(free_list_t* fl, size_t size, fl_policy policy)
+{
   return free_list_alloc_align(fl, size, policy, DEFAULT_ALIGN);
 }
 
-void* free_list_alloc_align(free_list_t* fl, size_t size, fl_policy policy, uintptr_t align) {
+void* free_list_alloc_align(free_list_t* fl, size_t size, fl_policy policy, uintptr_t align)
+{
   if (!fl) return NULL;
 
   // // TODO: Remove!
@@ -392,7 +421,8 @@ void* free_list_alloc_align(free_list_t* fl, size_t size, fl_policy policy, uint
   return memset(ptr, 0, size);
 }
 
-void free_list_free(free_list_t* fl, void* element, size_t element_size) {
+void free_list_free(free_list_t* fl, void* element, size_t element_size)
+{
   free_list_free_align(fl, element, element_size, DEFAULT_ALIGN);
 }
 
@@ -462,7 +492,8 @@ void  free_list_free_align(free_list_t* fl, void* element, size_t element_size, 
   ++call;
 }
 
-void free_list_free_all(free_list_t* fl) {
+void free_list_free_all(free_list_t* fl)
+{
   free_list_free_all_align(fl, DEFAULT_ALIGN);
 }
 
@@ -474,7 +505,8 @@ void free_list_free_all_align(free_list_t* fl, uintptr_t align) {
   first_hdr->linked_hdr = NULL;
 }
 
-void free_list_find_first(free_list_t* fl, size_t size, fl_hdr_t** found_hdr, fl_hdr_t** prev_hdr) {
+void free_list_find_first(free_list_t* fl, size_t size, fl_hdr_t** found_hdr, fl_hdr_t** prev_hdr)
+{
   fl_hdr_t* hdr = fl_first_hdr(fl);
   fl_hdr_t* phdr = NULL;
 
@@ -497,7 +529,8 @@ void free_list_find_first(free_list_t* fl, size_t size, fl_hdr_t** found_hdr, fl
   *prev_hdr = phdr;
 }
 
-void free_list_find_best(free_list_t* fl, size_t size, fl_hdr_t** found_hdr, fl_hdr_t** prev_hdr) {
+void free_list_find_best(free_list_t* fl, size_t size, fl_hdr_t** found_hdr, fl_hdr_t** prev_hdr)
+{
   fl_hdr_t* first_hdr = fl_first_hdr(fl); // TODO: Remove (see below).
   fl_hdr_t* hdr = fl_first_hdr(fl);
   fl_hdr_t* phdr = NULL;
